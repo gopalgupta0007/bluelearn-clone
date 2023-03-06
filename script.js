010f2d91cd6ba5069f24acabdd425d92d9d908a0
gopalgupta0007 <guptagopal18082003@gmail.com>
2023-03-06 15:36:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,2 @@
+const goToPlayStore = () => window.location.href = 'https://play.google.com/store/apps/details?id=com.bluelearn' 
+const goToAppleStore = () => window.location.href = 'https://apps.apple.com/in/app/bluelearn/id1659606724' 
